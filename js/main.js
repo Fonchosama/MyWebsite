5c9b1074a9928d5bbcf9ebf9feadb05a275201fd
GianBo <gianbo251@gmail.com>
2025-03-31 13:58:42 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 
-// Function that let the content of the about page buttons being showed// 
+// // Function that let the content of the about page buttons being showed// 
 
-function toggleContent(id) {
-    const content = document.getElementById(id);
+// function toggleContent(id) {
+//     const content = document.getElementById(id);
     
-    // Alterna entre mostrar y ocultar
-    if (content.style.display === "none" || content.style.display === "") {
-        content.style.display = "block";
-    } else {
-        content.style.display = "none";
-    }
-}
+//     // Alterna entre mostrar y ocultar
+//     if (content.style.display === "none" || content.style.display === "") {
+//         content.style.display = "block";
+//     } else {
+//         content.style.display = "none";
+//     }
+// }
